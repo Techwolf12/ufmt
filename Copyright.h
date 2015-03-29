eacff85f443b100f7462ae92a5017662636c5d2f
Angelo Geels <spansjh@gmail.com>
2015-03-29 17:09:27 +0200

    Added copyright

---
+++ Copyright.h
@@ -0,0 +1,7 @@
+/**
+ * Ultimate-Formatter is licensed under the GNU GPL v2.
+ * Please see the LICENSE file for more information.
+ *
+ * (c) Christiaan de Die <contact@techwolf12.nl>
+ * (c) Angelo Geels <spansjh@gmail.com>
+ */
